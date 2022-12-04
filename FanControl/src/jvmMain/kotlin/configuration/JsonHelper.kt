8a618456c7fbae58dc8e3470db81ebb0b8f56493
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2022-12-04 10:51:01 +0100

    create function

---
+++ FanControl/src/jvmMain/kotlin/configuration/JsonHelper.kt
@@ -0,0 +1,71 @@
+package configuration
+
+import androidx.compose.ui.res.useResource
+import org.json.JSONArray
+import org.json.JSONObject
+import org.json.JSONTokener
+
+
+enum class JsonObjectType {
+    ARRAY,
+    OBJECT,
+    VALUE
+}
+
+private const val STRING_FILE_NAME = ""
+
+private const val DEFAULT_LANGUE = "en"
+
+class JsonHelper {
+
+    companion object {
+        private val _rootJsonObject: JSONObject
+
+        init {
+            val string = useResource("values/$STRING_FILE_NAME") {
+                it.bufferedReader().readText()
+            }
+            _rootJsonObject = JSONTokener(string).nextValue() as JSONObject
+
+        }
+
+        private fun experimentalGetString(path: List<Pair<JsonObjectType, String>>): String {
+
+            var tempJSONObject: JSONObject = JsonHelper._rootJsonObject
+            var tempJsonArray: JSONArray? = null
+
+            var lastUseObjectType: JsonObjectType = JsonObjectType.OBJECT
+
+            for (p in path) {
+                when (p.first) {
+                    JsonObjectType.VALUE -> {
+                        return tempJSONObject.getString(p.second)
+                    }
+
+                    JsonObjectType.ARRAY -> {
+                        tempJsonArray = when (lastUseObjectType) {
+                            JsonObjectType.ARRAY -> tempJsonArray!!.getJSONArray(p.second.toInt())
+                            JsonObjectType.OBJECT -> tempJSONObject.getJSONArray(p.second)
+                            else -> {
+                                throw Exception("Resources: lastUseObjectType is a value")
+                            }
+                        }
+                        lastUseObjectType = JsonObjectType.ARRAY
+                    }
+
+                    JsonObjectType.OBJECT -> {
+                        tempJSONObject = when (lastUseObjectType) {
+                            JsonObjectType.ARRAY -> tempJsonArray!!.getJSONObject(p.second.toInt())
+                            JsonObjectType.OBJECT -> tempJSONObject.getJSONObject(p.second)
+                            else -> {
+                                throw Exception("Resources: lastUseObjectType is a value")
+                            }
+                        }
+                        lastUseObjectType = JsonObjectType.OBJECT
+                    }
+                }
+            }
+            throw Exception("Value not found")
+        }
+    }
+}
--- FanControl/src/jvmMain/kotlin/ui/utils/Resources.kt
+++ FanControl/src/jvmMain/kotlin/ui/utils/Resources.kt
@@ -1,30 +1,25 @@
 package ui.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.loadSvgPainter
 import androidx.compose.ui.res.useResource
-import org.json.JSONArray
 import org.json.JSONObject
 import org.json.JSONTokener
 
 
-enum class JsonObjectType {
-    ARRAY,
-    OBJECT,
-    VALUE
-}
-
-
 private const val STRING_FILE_NAME = "strings.json"
 
-private const val DEFAULT_LANGUE = "en"
+private const val DEFAULT_LANGUAGE = "en"
+
 
 class Resources {
 
     companion object {
 
-        var langue: String = DEFAULT_LANGUE
+        var langue: String = DEFAULT_LANGUAGE
 
         private val _rootJsonObject: JSONObject
 
@@ -35,52 +30,20 @@ class Resources {
             _rootJsonObject = JSONTokener(string).nextValue() as JSONObject
         }
 
-
         fun getString(id: String): String {
             return _rootJsonObject.getString(id)
         }
 
         @Composable
-        fun getIcon(id: String): Painter = painterResource("drawable/$id-48px.xml")
-
-
-        private fun experimentalGetString(path: List<Pair<JsonObjectType, String>>): String {
-
-            var tempJSONObject: JSONObject = _rootJsonObject
-            var tempJsonArray: JSONArray? = null
-
-            var lastUseObjectType: JsonObjectType = JsonObjectType.OBJECT
-
-            for (p in path) {
-                when (p.first) {
-                    JsonObjectType.VALUE -> {
-                        return tempJSONObject.getString(p.second)
-                    }
-
-                    JsonObjectType.ARRAY -> {
-                        tempJsonArray = when (lastUseObjectType) {
-                            JsonObjectType.ARRAY -> tempJsonArray!!.getJSONArray(p.second.toInt())
-                            JsonObjectType.OBJECT -> tempJSONObject.getJSONArray(p.second)
-                            else -> {
-                                throw Exception("Resources: lastUseObjectType is a value")
-                            }
-                        }
-                        lastUseObjectType = JsonObjectType.ARRAY
-                    }
-
-                    JsonObjectType.OBJECT -> {
-                        tempJSONObject = when (lastUseObjectType) {
-                            JsonObjectType.ARRAY -> tempJsonArray!!.getJSONObject(p.second.toInt())
-                            JsonObjectType.OBJECT -> tempJSONObject.getJSONObject(p.second)
-                            else -> {
-                                throw Exception("Resources: lastUseObjectType is a value")
-                            }
-                        }
-                        lastUseObjectType = JsonObjectType.OBJECT
-                    }
+        fun getIcon(id: String): Painter {
+            val density = LocalDensity.current // to calculate the intrinsic size of vector images (SVG, XML)
+            return remember {
+                useResource("resources/drawable/$id.svg") {
+                    loadSvgPainter(it, density)
                 }
             }
-            throw Exception("Value not found")
         }
+
+
     }
 }
