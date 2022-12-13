b87ba792dd3e0b4a0ed3fad721d4845bf52a2e9c
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2022-12-13 02:08:28 +0100

    upgrade add Item
    Todo:
    mettre une list de control existant au lieu de par default dans Add item

--- FanControl/src/jvmMain/kotlin/ui/screen/addItem/AddItemViewModel.kt
+++ FanControl/src/jvmMain/kotlin/ui/screen/addItem/AddItemViewModel.kt
@@ -1,3 +1,40 @@
 package ui.screen.addItem
 
-class AddItemViewModel
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import model.ItemType
+import model.item.BehaviorItem
+import model.item.ControlItem
+
+class AddItemViewModel(
+    private val _behaviorItemList: MutableStateFlow<SnapshotStateList<BehaviorItem>> = State._behaviorItemList,
+    private val _controlItemList: MutableStateFlow<SnapshotStateList<ControlItem>> = State._controlItemList,
+
+    ) {
+    fun addBehavior() {
+        _behaviorItemList.update {
+            it.add(
+                BehaviorItem(
+                    name = "Behavior",
+                    type = ItemType.BEHAVIOR
+                )
+            )
+            it
+        }
+    }
+
+    fun addControl() {
+        _behaviorItemList.update {
+            it.add(
+                ControlItem(
+                    name = "Control",
+                    type = ItemType.CONTROL
+                )
+            )
+            it
+        }
+    }
+}
+
+
