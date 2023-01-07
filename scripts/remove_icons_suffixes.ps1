396214470527f14295b70ca135d00c8b99b005dc
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-01-07 06:23:16 +0100

    Update remove_icons_suffixes.ps1

--- scripts/remove_icons_suffixes.ps1
+++ scripts/remove_icons_suffixes.ps1
@@ -1,16 +1,24 @@
-# Ce script renomme tous les fichiers dans le répertoire courant en enlevant le suffixe "_FILL"
 Set-Location "./../FanControl/src/jvmMain/resources/drawable"
+
+# Récupération de la liste des fichiers
 $files = Get-ChildItem -Recurse
+
+# Parcours de chaque fichier
 foreach ($file in $files)
 {
-    $parts = $file.Name.Split('_FILL')
+    # Découpage du nom du fichier en deux parties
+    $parts = $file -split '_FILL'
+
+    # Si le tableau contient deux éléments, cela signifie que le motif a été trouvé
     if ($parts.Count -eq 2)
     {
-        Write-Output "$parts"
-        $newName = $parts[0].Insert($parts[0].Length, '.svg')
+        # Réassemblage du nom du fichier avec l'extension .svg
+        $newName = $parts[0] + '.svg'
+
+        # Renommage du fichier
         Rename-Item -Path $file.FullName -NewName $newName
     }
 }
 
-
+# Retour au répertoire de départ
 Set-Location $PSScriptRoot
