4c9bca8425cc316c3d8051152028a73f2eb19f02
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-01-08 02:52:01 +0100

    init

---
+++ Library/Windows/Hardware/CppProxy/x64/Debug/.NETCoreApp,Version=v7.0.AssemblyAttributes.cpp
@@ -0,0 +1,2 @@
+#using <mscorlib.dll>
+[assembly: System::Runtime::Versioning::TargetFrameworkAttribute(L".NETCoreApp,Version=v7.0", FrameworkDisplayName=L".NET 7.0")];
--- Library/Windows/HardwareWrapper/HardwareLib/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HardwareLib
-{
-    public class Class1
-    {
-
-    }
-}
