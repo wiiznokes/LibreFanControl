b019e73154653defb1d574bfbe26b14d21780815
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-01-29 23:12:50 +0100

    add path to execute daemon
    an api should be good

--- FanControl/src/jvmMain/kotlin/external/windows/ExternalWindows.kt
+++ FanControl/src/jvmMain/kotlin/external/windows/ExternalWindows.kt
@@ -2,17 +2,23 @@ package external.windows
 
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import external.External
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import model.hardware.Sensor
 import model.item.control.Control
+import java.io.File
 import java.io.InputStream
 import java.net.Socket
 
 class ExternalWindows : External {
 
+    private lateinit var process: Process
+
     private lateinit var client: Socket
     private var inputStream: InputStream? = null
 
     private val byteArray = ByteArray(1024)
+
 
     override fun start(
         fanList: SnapshotStateList<Sensor>,
@@ -20,6 +26,14 @@ class ExternalWindows : External {
         controlList: SnapshotStateList<Control>,
         controlChangeList: SnapshotStateList<Boolean>
     ) {
+        val path = File(System.getProperty("compose.application.resources.dir"))
+            .resolve("net6.0-windows/win-x64/HardwareDaemon.exe").path
+
+        process = ProcessBuilder(path).start()
+
+        runBlocking {
+            delay(2000L)
+        }
 
         client = Socket("::1", 11000)
         inputStream = client.getInputStream()
@@ -29,6 +43,7 @@ class ExternalWindows : External {
     override fun stop() {
         client.close()
     }
+
 
     override fun setFanList(fanList: SnapshotStateList<Sensor>) {
         val bytesRead = inputStream?.read(byteArray)
@@ -43,7 +58,6 @@ class ExternalWindows : External {
     }
 
     override fun setTempList(tempList: SnapshotStateList<Sensor>) {
-
     }
 
     override fun setControlList(controlList: SnapshotStateList<Control>) {
