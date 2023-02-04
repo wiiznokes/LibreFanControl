19f8fef24dfba00e3a048859f289b81a39472bce
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-02-04 18:10:04 +0100

    Update generation_proto_file.ps1

--- proto/generation_proto_file.ps1
+++ proto/generation_proto_file.ps1
@@ -1,8 +1,11 @@
 $protocPath = $PSScriptRoot + "/protoc-21.12-win64/bin/protoc.exe"
-$protoFilePath = $PSScriptRoot + "/data.proto"
 
+$protoFiles = Get-ChildItem -Path . -Filter *.proto -File | Select-Object -ExpandProperty FullName
 
 $csharpOutput = $PSScriptRoot + "/../Library/Windows/HardwareLib/HardwareDaemon/"
 $kotlinOutput = $PSScriptRoot + "/../FanControl/src/jvmMain/java/"
- 
-& "$protocPath" "-I" "$PSScriptRoot" "--csharp_out" "$csharpOutput" "--java_out" "$kotlinOutput" "$protoFilePath"
+
+
+foreach ($protoFile in $protoFiles) {
+    & "$protocPath" "-I" "$PSScriptRoot" "--csharp_out" "$csharpOutput" "--java_out" "$kotlinOutput" "$protoFile"
+}
