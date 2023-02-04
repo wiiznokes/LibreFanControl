24eca1352ca87cb2cfec97f2a761ceff207856f8
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-02-04 17:48:04 +0100

    Delete generation_proto_file.ps1

--- scripts/generation_proto_file.ps1
+++
@@ -1,5 +0,0 @@
-$protocPath = "/../"
-$PSScriptRoot
-
-
-.\HardwareDaemon\proto\protoc-21.12-win64\bin\protoc.exe .\HardwareDaemon\proto\data.proto --csharp_out=.\HardwareDaemon\proto\ --java_out=.\..\src\jvmMain\kotlin\
