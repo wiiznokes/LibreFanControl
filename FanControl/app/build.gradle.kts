30a0ce413f2273a70ddcc0a266d262a5b08631fd
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-02-07 01:06:31 +0100

    Update build.gradle.kts

--- FanControl/app/build.gradle.kts
+++ FanControl/app/build.gradle.kts
@@ -42,6 +42,8 @@ kotlin {
 
                 // use to debug the lib locally
                 //implementation("com.example:setting-sliding-windows-jvm:2.0.1")
+
+                implementation(project(":proto"))
             }
         }
 
