f6ee0c901cfc6003875c60e7ab4a7ecf0d0b5965
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-02-08 05:13:24 +0100

    publish settings

--- FanControl/app/build.gradle.kts
+++ FanControl/app/build.gradle.kts
@@ -34,7 +34,7 @@ kotlin {
                 implementation(compose.desktop.currentOs)
 
                 implementation("org.jetbrains.compose.material3:material3-desktop:${project.property("compose.version")}")
-                //implementation("com.github.wiiznokes:setting-sliding-windows:${project.property("compose.version")}")
+                implementation("com.github.wiiznokes:setting-sliding-windows:${project.property("settings.version")}")
 
                 implementation("org.json:json:20220924")
                 implementation("com.google.protobuf:protobuf-java:${project.property("protobuf.version")}")
@@ -42,7 +42,7 @@ kotlin {
 
 
                 // use to debug the lib locally
-                implementation("com.example:setting-sliding-windows-jvm:${project.property("compose.version")}")
+                //implementation("com.example:setting-sliding-windows-jvm:${project.property("settings.version")}")
             }
         }
 
