5de2b9e266877a8cf3e69825a4159929baadcc88
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-02-11 03:23:54 +0100

    change name

--- FanControl/app/src/jvmMain/kotlin/proto/CrossApi.kt
+++ FanControl/app/src/jvmMain/kotlin/proto/CrossApi.kt
@@ -1,27 +1,18 @@
 package proto
 
 import io.grpc.ManagedChannel
-import proto.generated.api.Api
+import proto.generated.api.PCrossApiGrpcKt
 import java.io.Closeable
 import java.util.concurrent.TimeUnit
 
-class Api {
-}
-
-/*
-
-class HelloWorldClient(
+class CrossApi(
     private val channel: ManagedChannel
-) : Closeable {
-    private val stub: GreeterCoroutineStub  = GreeterCoroutineStub(channel)
+) : Closeable{
+    private val stub: PCrossApiGrpcKt.PCrossApiCoroutineStub  = GreeterCoroutineStub(channel)
 
-    suspend fun greet(name: String) {
 
-    }
 
     override fun close() {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
     }
 }
-
- */
