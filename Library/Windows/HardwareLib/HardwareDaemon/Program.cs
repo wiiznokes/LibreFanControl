758c1b42698aabf09dae5e4c049ab895b6cac81b
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-02-11 11:55:04 +0100

    add some confs files

--- Library/Windows/HardwareLib/HardwareDaemon/Program.cs
+++ Library/Windows/HardwareLib/HardwareDaemon/Program.cs
@@ -36,6 +36,7 @@ internal static class Program
         builder.Services.AddGrpc();
         var app = builder.Build();
         app.MapGrpcService<CrossApi>();
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
         app.Run();
 
