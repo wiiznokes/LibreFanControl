38bf42d86e44d639264d67e1a99d5fd6051a93d4
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-02-12 03:23:12 +0100

    static mgl

--- Library/Windows/HardwareLib/HardwareDaemon/State.cs
+++ Library/Windows/HardwareLib/HardwareDaemon/State.cs
@@ -6,7 +6,7 @@ using HardwareDaemon.Item.Behavior;
 
 namespace HardwareDaemon;
 
-public class MyDataStore
+public static class MyDataStore
 {
     private static readonly ConcurrentDictionary<int, BaseControl> HControls = new ConcurrentDictionary<int, BaseControl>();
     private static readonly ConcurrentDictionary<int, BaseSensor> HTemps = new ConcurrentDictionary<int, BaseSensor>();
@@ -20,107 +20,107 @@ public class MyDataStore
 
     private static readonly ConcurrentDictionary<int, IBaseItem> UpdateList = new ConcurrentDictionary<int, IBaseItem>();
 
-    public void AddHControl(BaseControl control)
+    public static void AddHControl(BaseControl control)
     {
         HControls.TryAdd(HControls.Count, control);
     }
 
-    public void AddHTemp(BaseSensor temp)
+    public static void AddHTemp(BaseSensor temp)
     {
         HTemps.TryAdd(HTemps.Count, temp);
     }
 
-    public void AddHFan(BaseSensor fan)
+    public static void AddHFan(BaseSensor fan)
     {
         HFans.TryAdd(HFans.Count, fan);
     }
 
-    public void AddControl(Control control)
+    public static void AddControl(Control control)
     {
         Controls.TryAdd(Controls.Count, control);
     }
 
-    public void AddBehavior(Behavior behavior)
+    public static void AddBehavior(Behavior behavior)
     {
         Behaviors.TryAdd(Behaviors.Count, behavior);
     }
 
-    public void AddCustomTemp(CustomTemp customTemp)
+    public static void AddCustomTemp(CustomTemp customTemp)
     {
         CustomTemps.TryAdd(CustomTemps.Count, customTemp);
     }
 
-    public void AddUpdate(IBaseItem update)
+    public static void AddUpdate(IBaseItem update)
     {
         UpdateList.TryAdd(UpdateList.Count, update);
     }
 
-    public IEnumerable<BaseControl> GetHControls()
+    public static IEnumerable<BaseControl> GetHControls()
     {
         return HControls.Values;
     }
 
-    public IEnumerable<BaseSensor> GetHTemps()
+    public static IEnumerable<BaseSensor> GetHTemps()
     {
         return HTemps.Values;
     }
 
-    public IEnumerable<BaseSensor> GetHFans()
+    public static IEnumerable<BaseSensor> GetHFans()
     {
         return HFans.Values;
     }
 
-    public IEnumerable<Control> GetControls()
+    public static IEnumerable<Control> GetControls()
     {
         return Controls.Values;
     }
 
-    public IEnumerable<Behavior> GetBehaviors()
+    public static IEnumerable<Behavior> GetBehaviors()
     {
         return Behaviors.Values;
     }
 
-    public IEnumerable<CustomTemp> GetCustomTemps()
+    public static IEnumerable<CustomTemp> GetCustomTemps()
     {
         return CustomTemps.Values;
     }
 
-    public IEnumerable<IBaseItem> GetUpdates()
+    public static IEnumerable<IBaseItem> GetUpdates()
     {
         return UpdateList.Values;
     }
 
-    public bool TryGetHControl(int index, out BaseControl control)
+    public static bool TryGetHControl(int index, out BaseControl control)
     {
         return HControls.TryGetValue(index, out control);
     }
 
-    public bool TryGetHTemp(int index, out BaseSensor temp)
+    public static bool TryGetHTemp(int index, out BaseSensor temp)
     {
         return HTemps.TryGetValue(index, out temp);
     }
     
-    public bool TryGetHFan(int index, out BaseSensor fan)
+    public static bool TryGetHFan(int index, out BaseSensor fan)
     {
         return HFans.TryGetValue(index, out fan);
     }
 
-    public bool TryGetControl(int index, out Control control)
+    public static bool TryGetControl(int index, out Control control)
     {
         return Controls.TryGetValue(index, out control);
     }
 
-    public bool TryGetBehavior(int index, out Behavior behavior)
+    public static bool TryGetBehavior(int index, out Behavior behavior)
     {
         return Behaviors.TryGetValue(index, out behavior);
     }
 
-    public bool TryGetCustomTemp(int index, out CustomTemp customTemp)
+    public static bool TryGetCustomTemp(int index, out CustomTemp customTemp)
     {
         return CustomTemps.TryGetValue(index, out customTemp);
     }
 
-    public bool TryGetUpdate(int index, out IBaseItem update)
+    public static bool TryGetUpdate(int index, out IBaseItem update)
     {
         return UpdateList.TryGetValue(index, out update);
     }
