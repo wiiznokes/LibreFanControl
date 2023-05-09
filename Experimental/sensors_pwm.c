4b29f2b48dc6a23be7d6298ee26a356ac3b0cb3d
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-05-09 03:14:56 +0200

    begin pwm

--- Experimental/sensors_pwm.c
+++ Experimental/sensors_pwm.c
@@ -1,3 +1,99 @@
-#include "pwm.h"
+#include "sensors_pwm.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <limits.h>
+
+const char *hwmon_path = "/sys/class/hwmon/hwmon6";
 
 
+
+
+
+pwm_feature *feat_list;
+pwm_sub_feature *sub_feat_list;
+
+int feat_list_count;
+int sub_feat_list_count;
+
+
+
+void pwm_fatal_error(const char *fun_name, char *msg) {
+    fprintf(stderr, "Fatal error in `%s': %s\n", fun_name, msg);
+	exit(1);
+}
+
+void add_pwm_feature(pwm_feature *feat) {
+    
+    pwm_feature *new_list = (pwm_feature*) realloc(feat_list, (feat_list_count + 1) * sizeof(pwm_feature));
+    if (!new_list) {
+        pwm_fatal_error(__func__, "can't realloc");
+    }
+
+    feat_list = new_list;
+   
+    feat_list[feat_list_count] = *feat;
+    feat_list_count++;
+    
+}
+
+void add_pwm_sub_feature(pwm_sub_feature *sub_feat) {
+    
+    pwm_sub_feature *new_list = (pwm_sub_feature*) realloc(sub_feat_list, (sub_feat_list_count + 1) * sizeof(pwm_sub_feature));
+    if (!new_list) {
+        pwm_fatal_error(__func__, "can't realloc");
+    }
+
+    sub_feat_list = new_list;
+   
+    sub_feat_list[sub_feat_list_count] = *sub_feat;
+    sub_feat_list_count++;
+    
+}
+
+
+
+
+int pwm_init() {
+
+    DIR *dir;
+    struct dirent *ent;
+    char file_path[PATH_MAX];
+
+    dir = opendir(hwmon_path);
+    if (dir == NULL) {
+        perror("Could not open hwmon directory");
+        return -1;
+    }
+
+    while ((ent = readdir(dir)) != NULL) {
+
+        /* Skip directories and symlinks */
+		if (ent->d_type != DT_REG)
+			continue;
+
+        sprintf(file_path, "%s/%s", hwmon_path, ent->d_name);
+
+        // Do something with file_path and its contents
+        // Example:
+        printf("File name: %s\n", ent->d_name);
+
+        FILE *fp = fopen(file_path, "r");
+        if (fp != NULL) {
+            char buffer[256];
+            while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+                printf("Content: %s", buffer);
+            }
+            fclose(fp);
+        } else {
+            perror("Could not open file");
+            return -1;
+        }
+        
+    }
+
+    closedir(dir);
+    return 0;
+
+}
--- Experimental/sensors_pwm.h
+++ Experimental/sensors_pwm.h
@@ -2,15 +2,97 @@
 #define SENSORS_PWM_H
 
 
-struct sensors_pwm {
+typedef enum pwm_sub_feature_type {
+    // SENSORS_SUBFEATURE_TEMP_AUTO_POINT_PWM,
+    // SENSORS_SUBFEATURE_TEMP_AUTO_POINT_TEMP,
+    // SENSORS_SUBFEATURE_TEMP_AUTO_POINT_TEMP_HYST,
+    //
+    //
+    SENSORS_SUBFEATURE_PWM_INPUT,
+    SENSORS_SUBFEATURE_PWM_ENABLE,
+    // SENSORS_SUBFEATURE_PWM_MODE,
+    // SENSORS_SUBFEATURE_PWM_FREQ,
+    // SENSORS_SUBFEATURE_PWM_AUTO_CHANNELS_TEMP,
+    // SENSORS_SUBFEATURE_PWM_AUTO_POINT_PWM,
+    // SENSORS_SUBFEATURE_PWM_AUTO_POINT_TEMP,
+    // SENSORS_SUBFEATURE_PWM_AUTO_POINT_TEMP_HYST,
+    //
+    //
+    // SENSORS_SUBFEATURE_PWM_CRIT_TEMP_TOLERANCE,
+    // SENSORS_SUBFEATURE_PWM_FLOOR,
+    // SENSORS_SUBFEATURE_PWM_STEP_UP_TIME,
+    // SENSORS_SUBFEATURE_PWM_STOP_TIME,
+    // SENSORS_SUBFEATURE_PWM_TARGET_TEMP,
+    // SENSORS_SUBFEATURE_PWM_TEMP_SEL,
+    // SENSORS_SUBFEATURE_PWM_TEMP_TOLERANCE,
+    // SENSORS_SUBFEATURE_PWM_WEIGHT_DUTY_BASE,
+    // SENSORS_SUBFEATURE_PWM_WEIGHT_DUTY_STEP,
+    // SENSORS_SUBFEATURE_PWM_WEIGHT_TEMP_SEL,
+    // SENSORS_SUBFEATURE_PWM_WEIGHT_TEMP_STEP,
+    // SENSORS_SUBFEATURE_PWM_WEIGHT_TEMP_STEP_BASE,
+    // SENSORS_SUBFEATURE_PWM_WEIGHT_TEMP_STEP_TOL
+
+} pwm_sub_feature_type;
+
+
+typedef struct pwm_feature {
 	char *name;
 	int number;
-	sensors_feature_type type;
-	/* Members below are for libsensors internal use only */
-	int first_subfeature;
-	int padding1;
-};
+
+} pwm_feature;
+
+typedef struct pwm_sub_feature {
+	char *name;
+	int number;
+	pwm_sub_feature_type type;
+} pwm_sub_feature;
 
 
+
+
+/*
+	return 0 on success, < 0 otherwise
+*/
+int pwm_init();
+
+
+/*
+	need to call pwm_init to reuse this lib after calling this function
+*/
+void pwm_clean_up();
+
+
+
+/* 
+	return NULL when nr is out of range 
+*/
+pwm_feature *pwm_get_features(int* nr);
+pwm_sub_feature *pwm_get_sub_features(pwm_feature *feat, int* nr);
+
+
+    
+char *get_pwm_feature_name(pwm_feature *feat);
+int get_pwm_feature_number(pwm_feature *feat);
+
+
+char *get_pwm_sub_feature_name(pwm_sub_feature *feat);
+int get_pwm_sub_feature_type(pwm_sub_feature *feat);
+int get_pwm_sub_feature_number(pwm_sub_feature *feat);
+
+/* 
+	value need to be between 0 and 255
+*/
+int pwm_set_value(pwm_feature *feat, int value);
+
+
+/* 
+	Fan speed control method:
+	0: no fan speed control (i.e. fan at full speed)
+	1: manual fan speed control enabled (using pwm[1-*])
+	2+: automatic fan speed control enabled
+	Check individual chip documentation files for automatic mode
+	details.
+*/
+int pwm_set_enable(pwm_feature *feat, int value);
 
 #endif
